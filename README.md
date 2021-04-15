e33edd30ebb598896e58c0b8f513c3eb0ba33b1d
Kayode Adechinan <kayode.adechinan@gmail.com>
2021-04-15 20:53:13 +0000

    initial setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Link common
+
+cd frontend npm link ../common
+
+# tuto
+
+- https://www.tsmean.com/articles/how-to-write-a-typescript-library/local-consumer/
+
+- https://www.tsmean.com/articles/how-to-write-a-typescript-library/
---
+++ common/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.randomString = void 0;
+var funtions_1 = require("./funtions");
+Object.defineProperty(exports, "randomString", { enumerable: true, get: function () { return funtions_1.randomString; } });
